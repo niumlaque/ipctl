e3492d343b59854339d50acd623725cc4a17534d
Niumlaque <niumlaque@gmail.com>
2024-03-10 21:30:53 +0900

    Add sample echo server utilizing this crate
    This commit adds a sample echo server demonstrating the usage of this crate.
    The echo server receives messages from ipctl-client via the crate and echoes them back unchanged.

---
+++ ipctl/examples/echo.rs
@@ -0,0 +1,14 @@
+/// First, start the sample echo-server with the following command:
+/// ```sh
+/// $ cargo run --example echo
+/// ```
+/// Then, send a message from ipctl:
+/// ```sh
+/// $ cargo run --bin ipctl Hello
+/// ```
+#[tokio::main]
+async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:60001".parse()?;
+    ipctl::Server::new(|x| x.to_string()).serve(addr).await?;
+    Ok(())
+}
